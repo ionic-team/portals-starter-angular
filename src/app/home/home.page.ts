e8aa75ac4778a435e40f636f176cd77391376859
Josh Thomas <jthoms1@gmail.com>
2023-02-03 14:30:35 -0600

    Add portals to the app.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { getInitialContext } from "@ionic/portals";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  styleUrls: ["home.page.scss"],
+  template: ` <ion-header
+      [translucent]="true"
+      [hidden]="!initialContext.showHeader"
+    >
+      <ion-toolbar>
+        <ion-title> Blank </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content [fullscreen]="true">
+      <ion-header collapse="condense" [hidden]="!initialContext.showHeader">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div id="container">
+        <pre>{{ initialContext | json }}</pre>
+      </div>
+    </ion-content>`,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  initialContext: any | null = null;
+
+  ngOnInit() {
+    this.initialContext = {
+      showHeader: false,
+      ...getInitialContext(),
+    };
+  }
 
   constructor() {}
-
 }
